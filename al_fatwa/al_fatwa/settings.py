6050c855649ea348c267dcf7fea2ccd401224627
Abdullateef2006 <ojugbelelateef2006@gmail.com>
2026-04-24 22:24:42 +0100

    remove environment variable logic

--- al_fatwa/al_fatwa/settings.py
+++ al_fatwa/al_fatwa/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0x9beu8!tla9%@l(%2o_y9@t!cdfgv&-u#66l5hih&kh^@5(d_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = False
 
 ALLOWED_HOSTS = ['alfatwa.pxxl.click', 'localhost', '127.0.0.1']
 
